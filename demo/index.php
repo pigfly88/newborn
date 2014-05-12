8af2bed02ca91c92db24926b4ffad5d48cea9a8e
zhuchenghui <328877098@qq.com>
2014-05-12 16:23:24 +0800

    remove

--- demo/index.php
+++
@@ -1,4 +0,0 @@
-<?php
-//项目入口文件
-define('APP_ROOT', dirname(__FILE__).'/');
-require_once '../NFS.php';
--- demo/public/index.php
+++
@@ -1,6 +0,0 @@
-<?php
-//项目入口文件
-define('APP_ROOT', dirname(__DIR__).'/');
-define('NFS_ROOT', dirname(APP_ROOT).'/');
-
-require_once NFS_ROOT.'NFS.php';
