c53c5b14bdbfe64101724b5796131556a6ccd8f0
justlikeheaven <328877098@qq.com>
2015-01-22 23:54:46 +0800

    ggg

--- base/View.php
+++
@@ -1,9 +0,0 @@
-<?php
-class View extends Component {
-	public static function load($var=array(), $view='', $ext='.html'){
-		$view = empty($view) ? NFS::$action : $view;
-		$data = $var;
-		include APP_ROOT.DS.VIEW_FOLDER_NAME.DS.$view.$ext;
-	}
-	
-}
