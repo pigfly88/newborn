923012e81224409c41f379516df89750eba4f295
unknown <BarryZhu@BY-01-0943.boyaa.com>
2015-06-19 15:32:00 +0800

    nfs

--- php/nfs/framework/base/view.php
+++
@@ -1,9 +0,0 @@
-<?php
-class view extends component {
-	public static function display($var=array(), $file='', $ext='.html'){
-		empty($file) && $file = nfs::$action;
-		$data = $var;
-		include VIEW_ROOT.$file.$ext;
-	}
-	
-}
